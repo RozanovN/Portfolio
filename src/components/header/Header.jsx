f6db9113e7fb1048bf19b989a04d759a99cdf84a
OceNick <ozcenick@gmail.com>
2022-01-04 04:40:41 -0800

    Add header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,23 @@
 import "./header.scss";
 
-export default function Header() {
+export default function Header({menuOpen, setMenuOpen}) {
   return (
-      <div className="header">
-
+    <div className={"header " + (menuOpen && "active") } id="header">
+      <div className="wrapper">
+        <div id="website-name">
+          <a href="#welcome">Portfolio</a>
+        </div>
+        <div id="nav-links">
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="#welcome">Experience</a>
+        </div>
+        <div className="hamburger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+          <span id="first-line"></span>
+          <span id="second-line"></span>
+          <span id="third-line"></span>
+        </div>
       </div>
+    </div>
   )
 }
