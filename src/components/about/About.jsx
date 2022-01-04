5f5a944226832d3f6ef4b43cf136b9206f9f5415
OceNick <ozcenick@gmail.com>
2022-01-03 16:54:31 -0800

    Add about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,27 @@
 import "./about.scss";
+import {Person, Mail, GitHub, LinkedIn} from "@material-ui/icons";
 
 export default function About() {
   return (
-      <div className="about">
-
+      <div className="about" id="about">
+        <div className="info-container">
+          <div className="info-line">
+            <Person className="icon"/>
+            <p>Nikolay Rozanov</p>
+          </div>
+          <div className="info-line">
+            <Mail className="icon"/>
+            <p>nrozanov@my.bcit.ca</p>
+          </div>
+          <div className="info-line">
+            <GitHub className="icon"/>
+            <a href="https://www.github.com/RozanovN" target="_blank">RozanovN</a>
+          </div>
+          <div className="info-line">
+            <LinkedIn className="icon"/>
+            <a href="https://linkedin.com/in/nikolay-rozanov" target="_blank">Nikolay Rozanov</a>
+          </div>
+        </div>
       </div>
   )
 }
